8622de5c7c246ce94f5371ac23453e1789988474
Charlie <charlie@Charlies-MacBook-Pro-7.local>
2016-12-12 14:32:30 +0800

    This is the inital files for the Human resource Management system for I.D.E.A in order to run this you need to have apache mysql and php environment

---
+++ idea/symfony/apps/orangehrm/templates/_copyright.php
@@ -0,0 +1,8 @@
+<?php 
+$version = '0.0.1';
+$prodName = 'Idea HRM';
+$copyrightYear = date('Y');
+
+?>
+<?php echo $prodName . ' ' . $version;?><br/>
+&copy; <?php echo $copyrightYear;?> <a href="http://github.com/cmaere" target="_blank">Charlie Maere</a>. All rights reserved.
---
+++ idea/symfony/apps/orangehrm/templates/_footer_copyright_social_links.php
@@ -0,0 +1,11 @@
+<?php
+$imagePath = theme_path("images/login");
+?>
+<div id="footer" >
+    <div>
+    <?php include_partial('global/copyright'); ?>
+    </div>
+    <div id="social-icons">
+       
+    </div>
+</div>
